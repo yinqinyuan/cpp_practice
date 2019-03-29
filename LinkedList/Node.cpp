3f1343e49ca94ce53bac071e8ed14c222b8afbe8
AD\qinyin <qinyuan.yin@zenuity.com>
2019-03-28 22:55:51 -0400

    Add LinkedList project

---
+++ LinkedList/Node.cpp
@@ -0,0 +1,5 @@
+//
+// Created by qinyin on 3/28/2019.
+//
+
+#include "Node.h"
---
+++ LinkedList/Node.h
@@ -0,0 +1,8 @@
+//
+// Created by qinyin on 3/28/2019.
+//
+
+#ifndef LINKEDLIST_NODE_H
+#define LINKEDLIST_NODE_H
+
+#endif //LINKEDLIST_NODE_H
